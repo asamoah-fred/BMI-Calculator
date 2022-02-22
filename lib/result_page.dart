6c505cb36282b8d73da486540cf5c9035d6dc1aa
asamoah-fred <asamoahgodfred42@gmail.com>
2022-02-22 17:19:38 +0000

    added Class

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class ResultPage extends StatefulWidget {
+  const ResultPage({Key? key}) : super(key: key);
+
+  @override
+  _ResultPageState createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
