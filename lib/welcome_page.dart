54b9ff5f1162d7e5325696bc16fce4db921b3734
asamoah-fred <asamoahgodfred42@gmail.com>
2022-02-25 09:24:00 +0000

    WelcomePage AppBar

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
 class WelcomePage extends StatelessWidget {
-  const WelcomePage({ Key? key }) : super(key: key);
+  const WelcomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(0, 10, 0, 10),
+        title: const Text(
+          "BMI CALCULATOR",
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        
+      ),
     );
   }
 }
